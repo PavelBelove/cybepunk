1a2469e21fbfcc84ef3948c15f2e12d08c69a31a
pavelbelove <agink@inbox.ru>
2022-06-28 19:05:43 +0300

    Life path in prosess

---
+++ classes/life_path.py
@@ -0,0 +1,93 @@
+from enum import Enum
+from re import I
+
+
+class Family(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Motivation(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Goals(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Friends(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Enemies(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Romance(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
+
+
+class Personality(Enum):
+    d1 = 'd1',
+    d2 = 'd2',
+    d3 = 'd3',
+    d4 = 'd4',
+    d5 = 'd5',
+    d6 = 'd6',
+    d7 = 'd7',
+    d8 = 'd8',
+    d9 = 'd9',
+    d10 = 'd10'
